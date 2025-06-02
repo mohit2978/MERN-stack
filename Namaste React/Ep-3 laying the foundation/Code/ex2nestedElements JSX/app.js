beb1da8da5f0683eb3ab9518a404ef462bd31b03
mohit2978 <mohitrakheja007@gmail.com>
2025-06-02 22:33:26 +0530

    Namaste react ep-3 done

--- Namaste React/Ep-3 laying the foundation/Code/ex2nestedElements JSX/app.js
+++ Namaste React/Ep-3 laying the foundation/Code/ex2nestedElements JSX/app.js
@@ -1,22 +1,33 @@
-
-import React from "react";
 import ReactDOM from "react-dom/client";
 
+let number=12;
 
+const spanTag=<span>Hi from span</span>
 
+// const titled=(
+//     <h1>Hello from react element{spanTag}</h1>
+// )
 
 const HeadingComponent=()=>{
     return <h1>heading component</h1>;
 }
 
-const HeadingComponent1=()=>(
-         <div id="parent">
-            <HeadingComponent/>
-            <h1>From heading component 1</h1>
-        </div>
-     )
+// const HeadingComponent1=function() {
+//     return( <div id="parent">
+//         <HeadingComponent/>
+//         <h2>Number={number*100}</h2>
+//         {titled}
+//         <h1>From heading component 1</h1>
+//     </div>);
+// }
 
+const titled1=(
+    <div>
+        <h1>Hello from react element{spanTag}</h1>
+        <HeadingComponent/>
+    </div>
+);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent1/>);
+root.render(titled1);
