1c18d2e071aa65bacd9718145aac1d27b2d433df
mohit2978 <mohitrakheja007@gmail.com>
2025-06-02 12:43:30 +0530

    Namaste react ep-3 40:00

---
+++ Namaste React/Ep-3 laying the foundation/Code/ex2nestedElements JSX/app.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+let jsxHeading=<h1 id="heading">Namaste react</h1>
+
+console.log(jsxHeading);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
