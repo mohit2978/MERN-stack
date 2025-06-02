f8160eca3e308e9ec6f36d72f07d90853035e70c
mohit2978 <mohitrakheja007@gmail.com>
2025-06-02 20:30:08 +0530

    Namaste react ep-3 01:30

--- Namaste React/Ep-3 laying the foundation/Code/ex2nestedElements JSX/app.js
+++ Namaste React/Ep-3 laying the foundation/Code/ex2nestedElements JSX/app.js
@@ -4,10 +4,19 @@ import ReactDOM from "react-dom/client";
 
 
 
-let jsxHeading=<h1 id="heading">Namaste react</h1>
 
-console.log(jsxHeading);
+const HeadingComponent=()=>{
+    return <h1>heading component</h1>;
+}
+
+const HeadingComponent1=()=>(
+         <div id="parent">
+            <HeadingComponent/>
+            <h1>From heading component 1</h1>
+        </div>
+     )
+
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent1/>);
